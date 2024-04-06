b7d6b021585a3437cfb4e84604cb6439935c74bd
dev-rox <142692001+dev-rox@users.noreply.github.com>
2024-04-06 17:07:38 -0300

    refactor: remove unused code

--- src/database/index.ts
+++
@@ -1,8 +0,0 @@
-// const Sequelize = require('sequelize');
-// const dbConfig = require('../config/database');
-// const Agendamento = require('../models/Agendamento');
-
-// const connection = new Sequelize(dbConfig);
-// Agendamento.init(connection);
-
-// module.exports = connection;
